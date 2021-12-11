f8996b3afb539b9bf801f8e4f8895843fa95ed73
TGBenW <dukariga@gmail.com>
2021-12-11 02:24:34 +0200

    fix

--- script.js
+++ script.js
@@ -4,8 +4,14 @@ const btn = document.getElementById('btn');
 const btnCircle = document.getElementById('e_btn');
 const textInput = document.getElementById('text');
 const rangeInput = document.getElementById('range');
+const rangeSpan = document.getElementById('range-span');
 const square = document.getElementById('square');
 const circle = document.getElementById('circle');
+
+const loader = function () {
+  rangeInput.value = 75;
+  loggerCircle();
+};
 
 const loggerColor = function () {
   if (textInput.value.trim() !== "") {
@@ -16,8 +22,10 @@ const loggerColor = function () {
 const loggerCircle = function () {
   circle.style.height = rangeInput.value + "%";
   circle.style.width = rangeInput.value + "%";
+  rangeSpan.textContent = rangeInput.value;
 };
 
+window.addEventListener('load', loader);
 btn.addEventListener('click', loggerColor); //1
 btnCircle.style.display = 'none'; //2
-rangeInput.addEventListener('change', loggerCircle); //3
+rangeInput.addEventListener('mousemove', loggerCircle); //3
